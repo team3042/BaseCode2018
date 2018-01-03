5155d00d8cbfbe2b8aba481e2fd13b122d635d3a
NewUser <NewUser@10.208.235.192>
2018-01-02 20:45:19 -0600

    Began RobotMap

--- src/org/usfirst/frc/team3042/robot/RobotMap.java
+++ src/org/usfirst/frc/team3042/robot/RobotMap.java
@@ -6,13 +6,31 @@ package org.usfirst.frc.team3042.robot;
  * floating around.
  */
 public class RobotMap {
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static int leftMotor = 1;
-    // public static int rightMotor = 2;
-    
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
+    // This is where we basically put all of the robot configuration information
+	
+	// Choose a robot
+	public static enum Robot{PRIMARY, SECONDARY, PROGRAMMING};
+	
+	private static Robot currentRobot = Robot.PRIMARY;
+	
+	private static final boolean IS_PRIMARY = currentRobot == Robot.PRIMARY;
+	private static final boolean IS_SECONDARY = currentRobot == Robot.SECONDARY;
+	private static final boolean IS_PROGRAMMING = currentRobot == Robot.PROGRAMMING;
+	
+	
+	// Robot dimensions
+	public static final double DRIVE_WHEEL_DIAMETER = 4.0;
+	public static final double WHEEL_BASE_WIDTH = (IS_PROGRAMMING)? 20.2 : 0.0 /* Measure the robot! */;
+	
+	// USB ports
+	public static final int JOYSTICK_LEFT = 0;
+	public static final int JOYSTICK_RIGHT = 1;
+	public static final int GAMEPAD = 2;
+	
+	// PWM ports
+	public static final int PAN_SERVO = 0;
+	public static final int TILT_SERVO = 1;
+	
+	// CAN IDs
+	public static final int LEFT_MOTOR = (IS_PRIMARY)? 0 : (IS_SECONDARY)? 0 : 0;
 }
